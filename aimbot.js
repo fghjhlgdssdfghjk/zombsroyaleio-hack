8889d36dbd6a8e2ce9739b30026e2b69148ae8f9
fahrennheit <46702201+fahrennheit@users.noreply.github.com>
2019-01-15 12:46:55 +0700

    Aimbot.js

---
+++ aimbot.js
@@ -0,0 +1,15 @@
+editAimToBe=equal(nearestActivate, ActivateFire)
+setObject{ifHang=2, setMouseAim=PlayerInertia
+
+if (activateSeePlayer),mouseClicked, adjustAimTo [
+
+PlayerInertia, PlayerX, PlayerY]
+
+recogFile1=overrideWebpackMap
+AimAlwaysAccurMode=true.html activated
+AimAlwaysAccurMode=(aimOverride=nearestActivate)
+triggerValue=x,y
+PlayerValueX=aimFireAt
+PlayerValueY=AimFireAt
+
+Complete-> ClosePackage.html, resolveTrigger(ShutdownTime=0)
