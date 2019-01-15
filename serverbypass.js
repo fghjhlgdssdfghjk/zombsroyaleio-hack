89f44eababab9900506c82c612320330b7189313
fahrennheit <46702201+fahrennheit@users.noreply.github.com>
2019-01-15 12:50:48 +0700

    serverbypass.js

---
+++ serverbypass.js
@@ -0,0 +1,12 @@
+CreateOverride(serverSide, response1, response2)
+var Cycle = NetworkBandwidth
+
+injectSourceCode = bypassShutdown, overShutdown{caseVer17)
+let BnetQcode equal q2, hang string value=337, shutdownTime=0
+
+float html.var1
+
+float html.injectDetection
+
+fileOverride > networkOverride
+
